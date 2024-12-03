bbd20e0dd187398fc71dff4deaabdd35f8840dd9
prachiig <prachi-ai-ig@outlook.com>
2024-12-03 11:23:30 +0530

    Add files via upload

--- prg6.java
+++ prg6.java
@@ -1,3 +1,9 @@
+// https://leetcode.com/problems/maximum-subarray/description/
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
 import java.util.Arrays;
 
 public class prg6{
@@ -31,6 +37,4 @@ public class prg6{
         }
         return res;
     }
-
-
 }
